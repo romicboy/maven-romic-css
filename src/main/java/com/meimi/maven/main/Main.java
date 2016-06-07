77a5d9772208e8cefdfb84d68babe1b26ebe6c90
romic <romicboy@163.com>
2016-06-07 18:33:07 +0800

    update

--- src/main/java/com/meimi/maven/main/Main.java
+++ src/main/java/com/meimi/maven/main/Main.java
@@ -1,18 +1,27 @@
 package com.meimi.maven.main;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 
 import com.meimi.maven.handler.CssHandler;
 import com.meimi.maven.main.entity.CssEntity;
 
 public class Main {
 
+	static File css = new File("style.css");
+	
 	public static void main(String[] args) {
 		CssHandler cssHandler = new CssHandler();
-		List<CssEntity> cssList = cssHandler.fromCss(new File("css/css.css"));
+		List<CssEntity> cssList = cssHandler.fromCss(css);
 		cssList = cssHandler.toRem(cssList);
-		System.out.println(cssHandler.output(cssList));
+		try {
+			IOUtils.write(cssHandler.output(cssList), new FileOutputStream(css));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
